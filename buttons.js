640ee6dc2f86490b51c633031bff620184007a5f
Auberon López <auberon.lopez@greenriver.edu>
2025-11-26 08:20:19 -0800

    in class

---
+++ buttons.js
@@ -0,0 +1,26 @@
+/*
+Some big block comment
+drtert
+ertert
+*/
+
+// Line comments
+
+// Sends a brief message to validate JS loading
+console.log("Hello!!!!!!!!");
+
+function logPress() {
+    console.log("Good job!");
+    console.log("You clicked a button!");
+}
+
+function showAlert() {
+    alert("Hello user!");
+}
+
+// make new function toggleDark
+// log "toggleDark called"
+function toggleDark() {
+    console.log("toggleDark called");
+    document.body.classList.toggle("dark-mode");
+}
